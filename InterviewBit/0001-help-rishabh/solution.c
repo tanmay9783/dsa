edaf8c5c35bf0323b6e2f552c1d12d09f05a6f40
tanmay9783 <tanmay9783@users.noreply.github.com>
2026-08-27 10:38:55 +0000

    Add InterviewBit: Help Rishabh!

---
+++ InterviewBit/0001-help-rishabh/solution.c
@@ -0,0 +1,10 @@
+/**
+ * @input A : Integer array
+ * @input n1 : Integer array's ( A ) length
+ * @input B : Integer
+ * 
+ * @Output Integer
+ */
+int solve(int* A, int n1, int B) {
+    wdgvtewagfiyw
+}
