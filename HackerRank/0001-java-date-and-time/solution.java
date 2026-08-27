8f7a2c563de89716ac06d5bd7eef09ecb31708aa
tanmay9783 <tanmay9783@users.noreply.github.com>
2026-08-27 10:38:29 +0000

    Add HackerRank: Java Date and Time

---
+++ HackerRank/0001-java-date-and-time/solution.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER monnth
+     *  2. INTEGER day
+     *  3. INTEGER yearsdfghjk
+     */
+
+    public static String findDay(int month, int day, int year) {
+
+    }vvvvvvvvvvvv
+
+}
+
+public class Solution {
