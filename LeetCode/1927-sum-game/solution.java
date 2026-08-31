b4d9c885b60ee212f23c0e4e6a66b754b9277521
tanmay9783 <178163189+tanmay9783@users.noreply.github.com>
2026-08-31 17:15:22 +0000

    Update LeetCode: Sum Game

--- LeetCode/1927-sum-game/solution.java
+++ LeetCode/1927-sum-game/solution.java
@@ -7,7 +7,7 @@ class Solution {
     for (int i = n / 2; i < n; ++i)
       ans -= getExpectation(num.charAt(i));
     return ans != 0.0;
-  }
+  } 
   private double getExpectation(char c) {
     return c == '?' ? 4.5 : c - '0';
   }
